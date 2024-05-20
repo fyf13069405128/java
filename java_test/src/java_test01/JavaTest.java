5faa529fd503e02884e4e5fbad0ae39ea6b21235
fyf <1533594796@qq.com>
2024-05-20 18:34:23 +0800

    test

---
+++ java_test/src/java_test01/JavaTest.java
@@ -0,0 +1,21 @@
+package java_test01;
+
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Integer:");
+        Integer I = new Integer(100);
+        System.out.println(I);
+        System.out.println(I.getClass());
+        System.out.println(I.hashCode());
+        System.out.println(I.toString());
+        System.out.println("String");
+        String s = "hello";
+        System.out.println(s);
+        System.out.println(s.getClass());
+        System.out.println(s.hashCode());
+        System.out.println(s.toString());
+    }
+}
+
+
---
+++ java_test/src/java_test02/JavaTest.java
@@ -0,0 +1,26 @@
+package java_test02;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    ArrayList a;
+    String s;
+    Collections c;
+    List l;
+    Set st;
+    Queue q;
+    ConcurrentHashMap chm;
+    Hashtable ht;
+    Object obj;
+    Integer I;
+
+
+
+
+
+
+}
